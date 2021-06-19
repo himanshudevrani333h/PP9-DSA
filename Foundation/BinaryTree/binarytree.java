9f54aeb10da5eda542de9d933b3220b35660b83c
Himanshu devrani <himanshudevrani33h@gmail.com>
2021-06-19 13:23:57 +0530

    update

--- Foundation/BinaryTree/binarytree.java
+++ Foundation/BinaryTree/binarytree.java
@@ -264,35 +264,216 @@ public class binarytree {
         return true;
     }
 
-
-    public static class isBSTpair{
-        int minele = (int)1e9;
-        int maxele = -(int)1e9;
+    public static class isBSTpair {
+        int minele = (int) 1e9;
+        int maxele = -(int) 1e9;
         boolean isbst = true;
     }
-    
 
-    public static isBSTpair ISBST(Node root){
-       
-        if(root == null){
+    public static isBSTpair ISBST(Node root) {
+
+        if (root == null) {
             return new isBSTpair();
         }
-       
-       isBSTpair left = ISBST(root.left);
-       if(!left.isbst) return left;
-       isBSTpair right = ISBST(root.right);
-       if(!right.isbst) return right;
 
-       isBSTpair self = new isBSTpair();
-       self.isbst = false;
-       if(left.minele < root.data && right.maxele > root.data){
-           self.minele = Math.min(left.minele, root.data);
-           self.maxele = Math.max(right.maxele, root.data);
-           self.isbst = true;
-       }
+        isBSTpair left = ISBST(root.left);
+        if (!left.isbst)
+            return left;
+        isBSTpair right = ISBST(root.right);
+        if (!right.isbst)
+            return right;
 
-       return self;
+        isBSTpair self = new isBSTpair();
+        self.isbst = false;
+        if (left.minele < root.data && right.maxele > root.data) {
+            self.minele = Math.min(left.minele, root.data);
+            self.maxele = Math.max(right.maxele, root.data);
+            self.isbst = true;
+        }
+
+        return self;
     }
 
+    public static int height_2(Node root) {
+        return root == null ? -1 : Math.max(height_2(root.left), height_2(root.right)) + 1;
+    }
+
+    public static boolean isBalanceTree(Node root) { // height of left - height of right node <= 1 && also >=-1;
+
+        if (root == null)
+            return true;
+
+        if (!isBalanceTree(root.left))
+            return false;
+        int left_height = height_2(root);
+
+        if (!isBalanceTree(root.right))
+            return false;
+        int right_height = height_2(root);
+
+        if (Math.abs(left_height - right_height) <= 1)
+            return true;
+
+        return false;
+    }
+
+    public static class balnced_tree_pair {
+        boolean isbalance = true;
+        int height = -1;
+    }
+
+    public static balnced_tree_pair isBalanceTree_2(Node root) {
+
+        if (root == null)
+            return new balnced_tree_pair();
+
+        balnced_tree_pair leftpair = isBalanceTree_2(root.left);
+        if (!leftpair.isbalance)
+            return leftpair;
+
+        balnced_tree_pair rightpair = isBalanceTree_2(root.right);
+        if (!rightpair.isbalance)
+            return rightpair;
+
+        balnced_tree_pair res = new balnced_tree_pair();
+        if (Math.abs(leftpair.height - rightpair.height) > 1) {
+            res.isbalance = false;
+            return res;
+        }
+
+        res.height = Math.max(leftpair.height, rightpair.height);
+
+        return res;
+    }
+
+    public static class tilt_pair {
+        int sum = 0;
+        int tiltSF = 0;
+    }
+
+    public static tilt_pair tiltOftree(Node root) {
+
+        if (root == null)
+            return new tilt_pair();
+
+        tilt_pair left = tiltOftree(root.left);
+        tilt_pair right = tiltOftree(root.right);
+
+        tilt_pair res = new tilt_pair();
+        res.tiltSF = left.tiltSF + right.tiltSF + Math.abs(left.sum - right.sum);
+        res.sum = left.sum + right.sum;
+
+        return res;
+    }
+
+    public static int[] tiltOftree_2(Node root) {
+
+        if (root == null)
+            return new int[2]; // idx 0 for tiltSF & idx 1 for sum
+
+        int[] left = tiltOftree_2(root.left);
+        int[] right = tiltOftree_2(root.right);
+
+        int res[] = new int[2];
+
+        res[0] = left[0] + right[0] + Math.abs(left[1] - right[1]);
+        res[1] = left[1] + right[1] + root.data;
+
+        return res;
+    }
+
+    public static class diaClass {
+        int highest_length = -1;
+        int diameter = 0;
+    }
+
+    public static diaClass diameterOftree(Node root) {
+        if (root == null)
+            return new diaClass();
+
+        diaClass left = diameterOftree(root.left);
+        diaClass right = diameterOftree(root.right);
+
+        diaClass res = new diaClass();
+        res.diameter = Math.max(Math.max(left.diameter, right.diameter),
+                left.highest_length + right.highest_length + 2);
+        res.highest_length = Math.max(left.highest_length, right.highest_length) + 1;
+        return res;
+
+    }
+
+    public static int diameterOftree_2(Node root) {
+        if (root == null)
+            return 0;
+
+        int leftdia = diameterOftree_2(root.left);
+        int rightdia = diameterOftree_2(root.right);
+
+        int left_height = height(root.left);
+        int right_height = height(root.right);
+
+        return Math.max(Math.max(leftdia, rightdia), left_height + right_height + 2);
+    }
+
+    public int[] diameterOfBinaryTree_02(Node root) {
+        if (root == null)
+            return new int[] { 0, -1 }; // {diameter,height}
+
+        int[] ld = diameterOfBinaryTree_02(root.left);
+        int[] rd = diameterOfBinaryTree_02(root.right);
+
+        int[] myAns = new int[2];
+        myAns[0] = Math.max(Math.max(ld[0], rd[0]), ld[1] + rd[1] + 2);
+        myAns[1] = Math.max(ld[1], rd[1]) + 1;
+
+        return myAns;
+    }
+
+    int diameter = 0;
+
+    public int diameterOfBinaryTree_03(Node root) {
+        if (root == null)
+            return -1;
+
+        int ld = diameterOfBinaryTree_03(root.left);
+        int rd = diameterOfBinaryTree_03(root.right);
+
+        diameter = Math.max(diameter, ld + rd + 2);
+        return Math.max(ld, rd) + 1;
+    }
+
+    public static class isbstPair {
+        boolean isbst = true;
+        int minElement = (int) 1e9;
+        int maxElement = -(int) 1e9;
+
+        Node maxbstNode = null;
+        int maxSize = 0;
+    }
+
+    public static isbstPair LargestBST(Node root) {
+
+        if(root== null) return new isbstPair();
+
+        isbstPair leftpair = LargestBST(root.left);
+        isbstPair rightpair = LargestBST(root.right);
+
+        isbstPair res = new isbstPair();
+        if (leftpair.isbst && rightpair.isbst && leftpair.maxElement < root.data && rightpair.minElement > root.data) {
+            res.isbst = true;
+            res.minElement = Math.min(leftpair.minElement, root.data);
+            res.maxElement = Math.max(rightpair.maxElement, root.data);
+
+            res.maxSize = leftpair.maxSize + rightpair.maxSize + 1;
+            res.maxbstNode = root;
+
+        }else{
+            res.isbst = false;
+            res.maxSize= Math.max(leftpair.maxSize, rightpair.maxSize);
+            res.maxbstNode = leftpair.maxSize >rightpair.maxSize ? leftpair.maxbstNode : rightpair.maxbstNode;
+        }
+
+        return res;
+    }
 
 }
