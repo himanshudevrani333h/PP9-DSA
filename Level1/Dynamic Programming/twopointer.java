e651d128360b80df997be4e440713fbaae2dda26
Himanshu Devrani <himanshudevrani333h@gmail.com>
2021-09-03 22:58:33 +0530

    dp

--- Level1/Dynamic Programming/twopointer.java
+++ Level1/Dynamic Programming/twopointer.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 class twopointer {
     // 1. Faith
@@ -214,9 +215,329 @@ class twopointer {
         return dp[SP];
     }
 
+    public static int num(String s, int idx, int dp[]) {
+        if (idx == s.length()) {
+            return dp[idx] = 1;
+        }
+        if (dp[idx] != -1)
+            return dp[idx];
+        int count = 0;
+        char ch = s.charAt(idx);
+        if (ch - '0' == 0) {
+            return dp[idx] = 0;
+        }
+        if (ch - '0' > 0 && ch - '0' <= 26) {
+
+            count += num(s, idx + 1, dp);
+        }
+        if (idx < s.length() - 1) {
+            int num = (s.charAt(idx) - '0') * 10 + s.charAt(idx + 1) - '0';
+            if (num <= 26) {
+                count += num(s, idx + 2, dp);
+            }
+        }
+
+        return dp[idx] = count;
+    }
+
+    public static int num_tabu(String s, int IDX, int dp[]) {
+        for (int idx = s.length(); idx >= 0; idx--) {
+            if (idx == s.length()) {
+                dp[idx] = 1;
+                continue;
+            }
+
+            int count = 0;
+            char ch = s.charAt(idx);
+            if (ch - '0' == 0) {
+                dp[idx] = 0;
+                continue;
+            }
+            if (ch - '0' > 0 && ch - '0' <= 26) {
+                count += dp[idx + 1]; // num(s, idx + 1, dp);
+            }
+            if (idx < s.length() - 1) {
+                int num = (s.charAt(idx) - '0') * 10 + s.charAt(idx + 1) - '0';
+                if (num <= 26) {
+                    count += dp[idx + 2]; // num(s, idx + 2, dp);
+                }
+            }
+
+            dp[idx] = count;
+        }
+
+        return dp[IDX];
+    }
+
+    public static int num_opti(String s) {
+        int a = 1, b = 0;
+        for (int idx = s.length() - 1; idx >= 0; idx--) {
+
+            char ch = s.charAt(idx);
+            int sum = 0;
+            if (ch - '0' != 0) {
+                sum += a;
+
+                if (idx < s.length() - 1) {
+                    int num = (s.charAt(idx) - '0') * 10 + s.charAt(idx + 1) - '0';
+                    if (num <= 26) {
+                        sum += b; // num(s, idx + 2, dp);
+                    }
+                }
+            }
+            b = a;
+            a = sum;
+
+        }
+
+        return a;
+    }
+
+    public static int numDecodings(String s) {
+        if (s.length() == 1) {
+            if (s.charAt(0) - '0' == 0)
+                return 0;
+        }
+        int dp[] = new int[s.length() + 1];
+        Arrays.fill(dp, -1);
+        return num(s, 0, dp);
+    }
+
+    static int mod = (int) 1e9 + 7;
+
+    public static long numDecodingII(String str, int idx, long dp[]) {
+        int n = str.length();
+        if (idx == n) {
+            return dp[idx] = 1;
+        }
+        if (dp[idx] != -1)
+            return dp[idx];
+        long count = 0;
+        char ch = str.charAt(idx);
+        if (ch == '0')
+            return dp[idx] = 0;
+        if (ch == '*') {
+            count = (count + 9 * numDecodingII(str, idx + 1, dp)) % mod;
+            if (idx < n - 1) {
+                char ch1 = str.charAt(idx + 1);
+                if (ch1 >= '0' && ch1 <= '6') {
+                    count = (count + 2 * numDecodingII(str, idx + 2, dp)) % mod;
+                } else if (ch1 >= '7' && ch1 <= '9') {
+                    count = (count + 1 * numDecodingII(str, idx + 2, dp)) % mod;
+                } else {
+                    count = (count + 15 * numDecodingII(str, idx + 2, dp)) % mod;
+                }
+            }
+        } else {
+            count = (count + 1 * numDecodingII(str, idx + 1, dp)) % mod;
+            if (idx < n - 1) {
+                char ch2 = str.charAt(idx + 1);
+                if (ch2 == '*' && ch == '1') {
+                    count = (count + 9 * numDecodingII(str, idx + 2, dp)) % mod;
+                } else if (ch2 == '*' && ch == '2') {
+                    count = (count + 6 * numDecodingII(str, idx + 2, dp)) % mod;
+                } else if (ch2 != '*') {
+                    int num = ch - '0' * 10 + ch2 - '0';
+                    if (num <= 26) {
+                        count = (count + 1 * numDecodingII(str, idx + 2, dp)) % mod;
+                    }
+                }
+            }
+        }
+
+        return dp[idx] = count;
+    }
+
+    public static int numDecodingII(String s) {
+
+        long dp[] = new long[s.length() + 1];
+        Arrays.fill(dp, -1);
+        long ans = numDecodingII(s, 0, dp);
+        for (long e : dp)
+            System.out.print(e + " ");
+        return (int) ans;
+    }
+
+    public static long numDecodingII_tabu(String str, int IDX, long dp[]) {
+        int n = str.length();
+
+        for (int idx = str.length(); idx >= 0; idx--) {
+            if (idx == n) {
+                dp[idx] = 1;
+                continue;
+            }
+            long count = 0;
+            char ch = str.charAt(idx);
+            if (ch == '0') {
+                dp[idx] = 0;
+                continue;
+            }
+            if (ch == '*') {
+                count = (count + 9 * dp[idx + 1]) % mod;
+                if (idx < n - 1) {
+                    char ch1 = str.charAt(idx + 1);
+                    if (ch1 >= '0' && ch1 <= '6') {
+                        count = (count + 2 * dp[idx + 2]) % mod;
+                    } else if (ch1 >= '7' && ch1 <= '9') {
+                        count = (count + 1 * dp[idx + 2]) % mod;
+                    } else {
+                        count = (count + 15 * dp[idx + 2]) % mod;
+                    }
+                }
+            } else {
+                count = (count + 1 * dp[idx + 1]) % mod;
+                if (idx < n - 1) {
+                    char ch2 = str.charAt(idx + 1);
+                    if (ch2 == '*' && ch == '1') {
+                        count = (count + 9 * dp[idx + 2]) % mod;
+                    } else if (ch2 == '*' && ch == '2') {
+                        count = (count + 6 * dp[idx + 2]) % mod;
+                    } else if (ch2 != '*') {
+                        int num = (ch - '0') * 10 + (ch2 - '0');
+                        if (num <= 26) {
+                            count = (count + 1 * dp[idx + 2]) % mod;
+                        }
+                    }
+                }
+            }
+
+            dp[idx] = count;
+        }
+
+        return dp[IDX];
+    }
+
+    public static long numDecodingOptiTwoPointer(String str) {
+        int n = str.length();
+        long a = 1, b = 0;
+        for (int idx = str.length() - 1; idx >= 0; idx--) {
+
+            long sum = 0;
+            char ch = str.charAt(idx);
+            if (ch != '0') {
+                if (ch == '*') {
+                    sum = (sum + 9 * a) % mod;
+                    if (idx < n - 1) {
+                        char ch1 = str.charAt(idx + 1);
+                        if (ch1 >= '0' && ch1 <= '6') {
+                            sum = (sum + 2 * b) % mod;
+                        } else if (ch1 >= '7' && ch1 <= '9') {
+                            sum = (sum + 1 * b) % mod;
+                        } else {
+                            sum = (sum + 15 * b) % mod;
+                        }
+                    }
+                } else {
+                    sum = (sum + 1 * a) % mod;
+                    if (idx < n - 1) {
+                        char ch2 = str.charAt(idx + 1);
+                        if (ch2 == '*' && ch == '1') {
+                            sum = (sum + 9 * b) % mod;
+                        } else if (ch2 == '*' && ch == '2') {
+                            sum = (sum + 6 * b) % mod;
+                        } else if (ch2 != '*') {
+                            int num = (ch - '0') * 10 + (ch2 - '0');
+                            if (num <= 26) {
+                                sum = (sum + 1 * b) % mod;
+                            }
+                        }
+                    }
+                }
+            }
+            b = a;
+            a = sum;
+
+        }
+        return a;
+    }
+
+    public static int goldMine(int[][] arr, int sr, int sc, int[][] dir, int[][] dp) {
+        int n = arr.length, m = arr[0].length;
+        if (sc == m - 1)
+            return dp[sr][sc] = arr[sr][sc];
+
+        if (dp[sr][sc] != -1)
+            return dp[sr][sc];
+
+        int maxGold = 0;
+        for (int[] d : dir) {
+            int r = sr + d[0];
+            int c = sc + d[1];
+
+            if (r >= 0 && c >= 0 && r < n && c < m) {
+                maxGold = Math.max(maxGold, goldMine(arr, r, c, dir, dp) + arr[sr][sc]);
+            }
+        }
+
+        return dp[sr][sc] = maxGold;
+    }
+
+    public static void goldMine() {
+        int[][] arr = { { 10, 33, 13, 15 }, { 22, 21, 04, 1 }, { 5, 0, 2, 3 }, { 0, 6, 14, 2 } };
+        int[][] dir = { { 0, 1 }, { 1, 1 }, { -1, 1 } };
+        int n = arr.length, m = arr[0].length;
+        int[][] dp = new int[n][m];
+
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        int maxGold = 0;
+        for (int r = 0; r < n; r++) {
+            maxGold = Math.max(maxGold, goldMine(arr, r, 0, dir, dp));
+        }
+
+        System.out.println(maxGold);
+    }
+
+    // Min Cost Path | DP-6
+    public static int MinCostPath(int[][] arr, int sr, int sc, int[][] dir, int[][] dp) {
+        int n = arr.length, m = arr[0].length;
+        if (sc == m - 1 && sr == n - 1)
+            return dp[sr][sc] = arr[sr][sc];
+
+        if (dp[sr][sc] != -1)
+            return dp[sr][sc];
+
+        int minPath = (int) 1e9;
+        for (int[] d : dir) {
+            int r = sr + d[0];
+            int c = sc + d[1];
+
+            if (r >= 0 && c >= 0 && r < n && c < m) {
+                minPath = Math.min(minPath, MinCostPath(arr, r, c, dir, dp) + arr[sr][sc]);
+            }
+        }
+
+        return dp[sr][sc] = minPath;
+    }
+
+    public static void MinCostPath() {
+        int cost[][] = { { 1, 2, 3 }, { 4, 8, 2 }, { 1, 5, 3 } };
+        int[][] dir = { { 1, 0 }, { 0, 1 }, { 1, 1 } };
+        int n = cost.length, m = cost[0].length;
+        int[][] dp = new int[n][m];
+
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        int minpath = (int) 1e9;
+        for (int r = 0; r < n; r++) {
+            minpath = Math.min(minpath, MinCostPath(cost, r, 0, dir, dp));
+        }
+
+        System.out.println(minpath);
+    }
+
     public static void main(String[] args) {
         int n = 7;
-        int[] dp = new int[n + 1];
+        // int[] dp = new int[n + 1];
+        // String str = "**";
+        // System.out.println( (int)numDecodingOptiTwoPointer(str));
+        MinCostPath();
+        // long dp[] = new long[str.length()+1];
+        // Arrays.fill(dp, -1);
+        // System.out.println( (int)numDecodingII_tabu("904",0,dp));
+        // for(long e: dp) System.out.print(e+" ");
         // System.out.println(fabo_(n));
         // System.out.println(fabo_memo(n, dp));
         // System.out.println(fabo_tabu(n, dp));
