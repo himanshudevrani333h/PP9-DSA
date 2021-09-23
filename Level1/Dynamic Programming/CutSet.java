a0bd184a1814f9c73caea0a38ea47a30b1452885
Himanshu Devrani <himanshudevrani333h@gmail.com>
2021-09-23 22:10:05 +0530

    update

--- Level1/Dynamic Programming/CutSet.java
+++ Level1/Dynamic Programming/CutSet.java
@@ -31,24 +31,24 @@ class CutSet {
         for (int gap = 1; gap <= EI; gap++)
             for (int si = 0, ei = gap; ei < n; si++, ei++) {
                 if (ei - si == 1) {
-                     dp[si][ei] = 0;
-                     continue;
+                    dp[si][ei] = 0;
+                    continue;
                 }
 
                 int min = (int) 1e9;
 
                 for (int cut = si + 1; cut < ei; cut++) {
 
-                    int leftRes = dp[si][cut]; //matrixMul(arr, si, cut, dp);
-                    int RightRes = dp[cut][ei]; //matrixMul(arr, cut, ei, dp);
+                    int leftRes = dp[si][cut]; // matrixMul(arr, si, cut, dp);
+                    int RightRes = dp[cut][ei]; // matrixMul(arr, cut, ei, dp);
 
                     min = Math.min(min, leftRes + RightRes + (arr[cut] * arr[si] * arr[ei]));
                 }
 
-                 dp[si][ei] = min;
+                dp[si][ei] = min;
             }
 
-            return dp[SI][EI];
+        return dp[SI][EI];
     }
 
     public static int matrixMul_tabu_path(int[] arr, int SI, int EI, int[][] dp) {
@@ -99,57 +99,87 @@ class CutSet {
         return res;
     }
 
-    // Minimum and Maximum values of an expression with * and +  <- GFG
+    // Minimum and Maximum values of an expression with * and + <- GFG
 
-    static  class pairMM{
-     int min = (int)1e9;
-     int max = 0;
-     pairMM(){
-         
-     };
+    static class pairMM {
+        int min = (int) 1e9;
+        int max = 0;
 
-     pairMM(int val){
-         this.min= this.max = val;
-     }
+        pairMM() {
+
+        };
+
+        pairMM(int val) {
+            this.min = this.max = val;
+        }
     }
 
-    public static pairMM evaluate(pairMM leftRes, pairMM rightRes, char operator){
-       pairMM res = new pairMM();
-       if(operator == '+'){
-         res.min = leftRes.min + rightRes.min;
-         res.max = leftRes.max + rightRes.max;
-       }else if( operator == '*'){
-        res.min = leftRes.min * rightRes.min;
-        res.max = leftRes.max * rightRes.max;
-       }
-       return res;
+    public static pairMM evaluate(pairMM leftRes, pairMM rightRes, char operator) {
+        pairMM res = new pairMM();
+        if (operator == '+') {
+            res.min = leftRes.min + rightRes.min;
+            res.max = leftRes.max + rightRes.max;
+        } else if (operator == '*') {
+            res.min = leftRes.min * rightRes.min;
+            res.max = leftRes.max * rightRes.max;
+        }
+        return res;
     }
-    public static pairMM mmValueOfExpression(String str, int si, int ei, pairMM dp[][]){
-        if( si == ei){
-            return dp[si][ei] = new pairMM(str.charAt(ei)- '0');
+
+    public static pairMM mmValueOfExpression(String str, int si, int ei, pairMM dp[][]) {
+        if (si == ei) {
+            return dp[si][ei] = new pairMM(str.charAt(ei) - '0');
         }
 
-        if(dp[si][ei] != null) return dp[si][ei];
+        if (dp[si][ei] != null)
+            return dp[si][ei];
         pairMM myRes = new pairMM();
-        for( int cut = si+1; cut<ei; cut +=2){
-            pairMM leftRes = mmValueOfExpression(str, si, cut-1, dp);
-            pairMM rightRes = mmValueOfExpression(str, cut+1, ei, dp);
-            pairMM evalRes = evaluate(leftRes,rightRes,str.charAt(cut));
+        for (int cut = si + 1; cut < ei; cut += 2) {
+            pairMM leftRes = mmValueOfExpression(str, si, cut - 1, dp);
+            pairMM rightRes = mmValueOfExpression(str, cut + 1, ei, dp);
+            pairMM evalRes = evaluate(leftRes, rightRes, str.charAt(cut));
 
-            myRes.min = Math.min(myRes.min,evalRes.min);
-            myRes.max = Math.max(myRes.max,evalRes.max);
+            myRes.min = Math.min(myRes.min, evalRes.min);
+            myRes.max = Math.max(myRes.max, evalRes.max);
         }
 
         return dp[si][ei] = myRes;
     }
-    public static void mmValueOfExpression(){
-      String str = "1+2*3+4*5";
-      int n = str.length();
-      pairMM dp[][] = new pairMM[n][n];
-      pairMM res = mmValueOfExpression(str, 0, n-1, dp);
-      System.out.println("Min val : " + res.min);
-      System.out.println("Max val : " + res.max);
+
+    public static void mmValueOfExpression() {
+        String str = "1+2*3+4*5";
+        int n = str.length();
+        pairMM dp[][] = new pairMM[n][n];
+        pairMM res = mmValueOfExpression(str, 0, n - 1, dp);
+        System.out.println("Min val : " + res.min);
+        System.out.println("Max val : " + res.max);
     }
+
+    // leetcode 312
+
+    public int maxCoins(int arr[], int si, int ei, int dp[][]) {
+        if (dp[si][ei] != 0)
+            return dp[si][ei];
+        int lele = si == 0 ? 1 : arr[si - 1];
+        int rele = ei == arr.length - 1 ? 1 : arr[ei + 1];
+
+        int max = 0;
+        for (int cut = si; cut <= ei; cut++) {
+            int leftCost = cut == si ? 0 : maxCoins(arr, si, cut - 1, dp);
+            int rightCost = cut == ei ? 0 : maxCoins(arr, cut + 1, ei, dp);
+
+            max = Math.max(max, leftCost + lele * arr[cut] * rele + rightCost);
+        }
+
+        return dp[si][ei] = max;
+    }
+
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int dp[][] = new int[n][n];
+        return maxCoins(nums, 0, n - 1, dp);
+    }
+
     public static void main(String[] args) {
         // int arr[] = { 40, 20, 30, 10, 30 };
         // System.out.println(matrixMultiplication(5, arr));
