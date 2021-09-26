70b881ca1a2eac148da9c50863ba62de448c88a7
Himanshu Devrani <himanshudevrani333h@gmail.com>
2021-09-26 11:24:26 +0530

    update

--- Level1/Dynamic Programming/stringset.java
+++ Level1/Dynamic Programming/stringset.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.List;
+
 class stringset {
 
     public static void display(int[] dp) {
@@ -552,6 +553,61 @@ class stringset {
             wordBreak_backEngg(s, 0, dp, len, wordDict, set, "", ans);
 
         return ans;
+    }
+
+    // Leetcode 10 <- Regular Expression matching
+    public String removeStars(String p) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(p.charAt(0));
+        int i = 1;
+
+        while (i < p.length()) {
+            while (i < p.length() && sb.charAt(sb.length() - 1) == '*' && p.charAt(i) == '*')
+                i++;
+
+            if (i < p.length())
+                sb.append(p.charAt(i));
+            i++;
+        }
+
+        return sb.toString();
+    }
+
+    public int isMatch(String s, String p, int n, int m, int[][] dp) {
+
+        if (n == 0 && m == 0)
+            return dp[n][m] = 1;
+        if (m == 0)
+            return dp[n][m] = 0;
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        char ch = n > 0 ? s.charAt(n - 1) : '$';
+        char ch2 = p.charAt(m - 1);
+
+        if (ch != '$' && (ch == ch2 || ch2 == '.')) {
+            return dp[n][m] = isMatch(s, p, n - 1, m - 1, dp);
+        } else if (ch2 == '*') {
+            boolean res = false;
+            if (n > 0 && m > 1 && ((p.charAt(m - 2) == ch) || p.charAt(m - 2) == '.'))
+                res = res || isMatch(s, p, n - 1, m, dp) == 1;
+
+            res = res || isMatch(s, p, n, m - 2, dp) == 1;
+
+            return dp[n][m] = res ? 1 : 0;
+        } else {
+
+            return dp[n][m] = 0;
+        }
+    }
+
+    public boolean isMatch(String s, String p) {
+        int n = s.length(), m = removeStars(p).length();
+        int dp[][] = new int[n + 1][m + 1];
+        for (int d[] : dp)
+            Arrays.fill(d, -1);
+        return isMatch(s, p, n, m, dp) == 1;
     }
     // // Raaju baandar question
     // public static void pattern(int n){
