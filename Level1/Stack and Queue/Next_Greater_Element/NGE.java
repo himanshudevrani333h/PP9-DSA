1ac7daf94dba23894138614f1154bda51724af17
Himanshu Devrani <himanshudevrani333h@gmail.com>
2021-10-05 18:32:33 +0530

    up

--- Level1/Stack and Queue/Next_Greater_Element/NGE.java
+++ Level1/Stack and Queue/Next_Greater_Element/NGE.java
@@ -13,12 +13,11 @@ public class NGE {
             while (st.getFirst() != -1 && arr[st.getFirst()] < arr[i])
                 ans[st.removeFirst()] = i;
 
-            st.addFirst(i);    
+            st.addFirst(i);
         }
 
         return ans;
     }
-
 
     public static int[] NSEonRight(int arr[]) {
         int n = arr.length;
@@ -30,7 +29,7 @@ public class NGE {
             while (st.getFirst() != -1 && arr[st.getFirst()] > arr[i])
                 ans[st.removeFirst()] = i;
 
-            st.addFirst(i);    
+            st.addFirst(i);
         }
 
         return ans;
@@ -42,16 +41,15 @@ public class NGE {
         Arrays.fill(ans, -1);
         LinkedList<Integer> st = new LinkedList<>();
         st.addFirst(-1);
-        for (int i = n-1; i >=0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             while (st.getFirst() != -1 && arr[st.getFirst()] < arr[i])
                 ans[st.removeFirst()] = i;
 
-            st.addFirst(i);    
+            st.addFirst(i);
         }
 
         return ans;
     }
-
 
     public static int[] NSEonLeft(int arr[]) {
         int n = arr.length;
@@ -59,11 +57,11 @@ public class NGE {
         Arrays.fill(ans, -1);
         LinkedList<Integer> st = new LinkedList<>();
         st.addFirst(-1);
-        for (int i = n-1; i >=0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             while (st.getFirst() != -1 && arr[st.getFirst()] > arr[i])
                 ans[st.removeFirst()] = i;
 
-            st.addFirst(i);    
+            st.addFirst(i);
         }
 
         return ans;
@@ -76,25 +74,86 @@ public class NGE {
         Arrays.fill(ans, -1);
         LinkedList<Integer> st = new LinkedList<>();
         st.addFirst(-1);
-        for (int i = 0; i <n * 2; i++) {
-         
-         
-            while (st.getFirst() != -1 && arr[st.getFirst()] < arr[i%n])
-                ans[st.removeFirst()] = arr[i%n];
-            
-            
-           if(i < n)
-            st.addFirst(i);    
+        for (int i = 0; i < n * 2; i++) {
+
+            while (st.getFirst() != -1 && arr[st.getFirst()] < arr[i % n])
+                ans[st.removeFirst()] = arr[i % n];
+
+            if (i < n)
+                st.addFirst(i);
         }
 
         return ans;
     }
 
-    
+    // https://practice.geeksforgeeks.org/problems/stock-span-problem-1587115621/1#
+    public static int[] calculateSpan(int[] arr, int n) {
+        int[] ans = new int[n];
+        LinkedList<Integer> st = new LinkedList<>();
+        st.addFirst(-1);
+
+        for (int i = 0; i < n; i++) {
+            while (st.getFirst() != -1 && arr[st.getFirst()] <= arr[i])
+                st.removeFirst();
+
+            ans[i] = i - st.getFirst();
+            st.addFirst(i);
+        }
+
+        return ans;
+    }
+
+    // leetcode 901
+
+    class StockSpanner {
+        int day = 0;
+        LinkedList<int[]> st = new LinkedList<>();
+
+        public StockSpanner() {
+            // {index, value}
+            st.addFirst(new int[] { -1, -1 });
+        }
+
+        public int next(int price) {
+            while (st.getFirst()[0] != -1 && st.getFirst()[1] <= price)
+                st.removeFirst();
+
+            int span = day - st.getFirst()[0];
+            st.addFirst(new int[] { day++, price });
+
+            return span;
+        }
+    }
+
+    // leetcode 20
+    public boolean isValid(String str) {
+        LinkedList<Character> st = new LinkedList<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{')
+                st.addFirst(ch);
+            else {
+                if (st.size() == 0)
+                    return false;
+                else if (st.getFirst() == '(' && ch != ')')
+                    return false;
+                else if (st.getFirst() == '[' && ch != ']')
+                    return false;
+                else if (st.getFirst() == '{' && ch != '}')
+                    return false;
+                else
+                    st.removeFirst();
+            }
+        }
+
+        return st.size() == 0;
+    }
+
     public static void main(String[] args) {
-        int arr[] ={2,1,3,1,2,4,5,9,6};
-        int ans [] = NSEonRight(arr);
-        for(int e : ans) System.out.print(e+" ");
+        int arr[] = { 2, 1, 3, 1, 2, 4, 5, 9, 6 };
+        int ans[] = NSEonRight(arr);
+        for (int e : ans)
+            System.out.print(e + " ");
     }
 
 }
